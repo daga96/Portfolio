082cf3ef675c8d9ab3fa02a2cd424ee847e79df5
Dagmara <44023957+daga96@users.noreply.github.com>
2022-02-14 15:05:31 +0900

    New laptop commit

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import Hero from "./Hero";
+import About from "./About";
+import Courses from "./Courses";
+import Contact from "./Contact";
+
+function Main() {
+  return (
+    <div className="Main">
+      <Hero />
+      <About />
+      <Courses />
+      <Contact />
+    </div>
+  );
+}
+export default Main;
