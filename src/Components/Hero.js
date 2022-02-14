994e8a6dae977418997927f6f5892d8714be3f4e
Dagmara <44023957+daga96@users.noreply.github.com>
2022-02-14 17:20:12 +0900

    edits

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,22 +1,18 @@
 import React from "react";
 import "./Hero.css";
 
-
-
 const Hero = () => {
- 
   return (
     <div className="Hero" id="hero">
-      <div className="text" >
+      <div className="text">
         <h5>Hello, I'm</h5>
         <h2>
           Dagmara
           <span>Dagmara</span>
           <span>Dagmara</span>
-          <span> frontend developer </span>
+          <span> Hi, I'am frontend developer and UX Designer. </span>
         </h2>
       </div>
-     
     </div>
   );
 };
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,18 +2,11 @@ import Hero from "./Hero";
 import About from "./About";
 import Courses from "./Courses";
 
-
-
 function Main() {
-
   return (
-    
-    <div className="Main" >
-    
-          <Hero />
-          <About />
-          <Courses />
-        
+    <div className="Main">
+      <Hero />
+      <About />
     </div>
   );
 }
